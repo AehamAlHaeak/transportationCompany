9e8332a6a6f094c17577fac4826afcdc44aaa1dc
AehamAlHaeak <www.haeak@gmal.com>
2025-04-10 16:36:20 +0300

    return tto previous

--- app/Http/Controllers/Worldcompany.php
+++ app/Http/Controllers/Worldcompany.php
@@ -74,7 +74,9 @@ class Worldcompany extends Controller
       return response()->json(["msg"=>"seccessfull",200]);
 
     }
-    public function addTryng(){
-        return "hellow i am from the second";
-    }
+
+   
+
+   
+
 }
